2c223520078563b88e44dcc3531dd56b3d4ade26
code-ashutosh <in.ashutoshtrivedi@gmail.com>
2019-12-31 18:45:52 +0530

    creates addition page

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.post("/",function(req, res){
     let bmi = weight/(height*height);// in Kg/metre2
     res.send("Your BMI is = "+bmi);
 });
+app.get("/addition.html",function(req, res){
+res.sendFile(__dirname+"/addition.html");
+});
+app.post("/addition",function(req, res){
+let num1=Number(req.body.n1);
+let num2=Number(req.body.n2);
+let result= num1+num2;
+res.send("Addition ="+result);
+});
 app.listen(3000,function(){
     console.log("Server started at port 3000");
 });
